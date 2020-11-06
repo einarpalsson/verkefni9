71806a73c00ad0ddb7ecf37c3837919e241a3d87
Einar Pálsson <epp5@hi.is>
2020-11-06 19:18:16 +0000

    fetch

--- src/index.js
+++ src/index.js
@@ -7,7 +7,62 @@ document.addEventListener('DOMContentLoaded', async () => {
   // Hér er allt „vírað“ saman
   const map = document.querySelector('.map');
   init(map);
- 
-  const createInfo = fetchEarthquakes();
+
+  function showResults(results) {
+    const [{
+      mag,
+      place,
+      time,
+      updated,
+      tz,
+      url,
+      detail,
+      felt,
+      cdi,
+      mmi,
+      alert,
+      status,
+      tsunami,
+      sig,
+      net,
+      code,
+      ids,
+      sources,
+      types,
+      nst,
+      dmin,
+      rms,
+      gap,
+      magType,
+      type,
+      title
+          }] = results;
+
+      const element = 
+        el('li', 
+            el('div'),
+            el('h2', title),
+            el('dl'),
+            el('dt', 'Tími'),
+            el('dd', time),
+            el('dt', 'Styrkur'),
+            el('dd', mag)
+        );
+    }
+
+  fetchEarthquakes()
+    .then((res) => {
+      if (!res.ok) {
+          throw new Error('None 200 status')
+      } else {
+        res.json()
+        .then ((data) => {
+          //console.log(data.features);
+          showResults(data.features);
+        });
+      }
+    })     
 });
+
+let results;
 
